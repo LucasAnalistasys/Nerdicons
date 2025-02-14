4fac98d478a8aebf63c54c8e6d61666318a5e157
LucasAnalistasys <lucascdarkron@gmail.com>
2025-02-14 08:55:18 -0300

    formatação link do ícone

--- public/js/linkCopy.js
+++ public/js/linkCopy.js
@@ -1,19 +1,12 @@
-// Seleciona todos os botões "copiar link" e adiciona um listener de evento
 document.querySelectorAll('.copy-link').forEach(button => {
     button.addEventListener('click', function() {
-        // Cria um input temporário
-        const tempInput = document.createElement('input');
-        tempInput.value = this.getAttribute('data-link');  // Pega o link armazenado no 'data-link'
-        document.body.appendChild(tempInput);
-        
-        // Seleciona o valor e copia
-        tempInput.select();
-        document.execCommand('copy');
-        
-        // Remove o input temporário
-        document.body.removeChild(tempInput);
-        
-        // Mostra um alerta de confirmação (ou você pode personalizar com um tooltip)
-        alert('Link copiado!');
+        const fileName = this.getAttribute('data-link').split('/').pop(); // Obtém apenas o nome do arquivo
+        const formattedLink = `<img src="https://nerdicons.leadspage.com.br/icons/${fileName}" />`;
+
+        navigator.clipboard.writeText(formattedLink).then(() => {
+            alert('Código copiado!');
+        }).catch(err => {
+            console.error('Erro ao copiar:', err);
+        });
     });
 });
